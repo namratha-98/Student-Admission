3ae37353e60cb25fe7e5d4c6c6a8a776b76b195c
namratha-98 <74010135+namratha-98@users.noreply.github.com>
2020-11-05 21:28:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 #include <assert.h>
 #include <stdio.h>
 #include "program.c"
@@ -50,7 +50,7 @@ int main()
 
 
 
-}
+        }
 
     }
 
